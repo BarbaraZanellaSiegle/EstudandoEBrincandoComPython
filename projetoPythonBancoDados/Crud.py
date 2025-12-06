a3daad226c24f592670d81b3264c23afa57f64c6
BarbaraZanellaSiegle <barbara_zanella@estudante.sc.senai.br>
2025-12-06 08:40:53 -0300

    Aplicando Funções no Crud

--- projetoPythonBancoDados/Crud.py
+++ projetoPythonBancoDados/Crud.py
@@ -1,12 +1,56 @@
-def create():
-    pass
+import sqlite3
+from time import sleep
 
-def read():
-    pass
+def chamafuncao():
+    print("Função chamada")
 
-def update():
-    pass
 
-def delete():
-    pass
+def listarTabela(cursor, conexao):
+    comando = "SELECT * FROM sqlite_master where type='table';"
+    cursor.execute(comando)
 
+    tabelas = cursor.fetchall()
+
+    if tabelas:
+        for tabela in tabelas:
+            print("LISTA DE TABELAS:")
+            print(f"Tabela:", tabela[1])
+    else:
+        print("Nenhuma Tabela encontrada no banco atual")
+
+
+def animacao():
+    for animacao in range(10):
+        sleep(0.3)
+        print("*")
+
+
+def interacaoBanco():
+
+    print("INTERAÇÃO COM O BANCO")
+    #Criando tabelas
+    criarTabela = int(input(f"Deseja criar uma tabela?\nDigite 1 para Sim e 2 para Não: "))
+    if criarTabela == 1:
+        print("Criando tabelas...")
+    
+    elif criarTabela == 2:
+        print("Ok, não será criado novas tabelas!")
+    
+    else:
+        print("Não foi informado uma opção valida!")
+
+
+    #Deletando tabelas
+    deletarTabela = int(input(f"Deseja deletar uma tabela?\nDigite 1 para Sim e 2 para Não: "))
+
+    if deletarTabela == 1:
+        print("Deletando tabelas...")
+    
+    elif deletarTabela == 2:
+        print("Ok, não será deletado nenhuma tabela!")
+    
+    else:
+        print("Não foi informado uma opção valida!")
+
+
+    loop = True
--- projetoPythonBancoDados/interacaoUsuario.py
+++ projetoPythonBancoDados/interacaoUsuario.py
@@ -1,8 +1,11 @@
 from time import sleep
 import sqlite3
+from Crud import chamafuncao, listarTabela, animacao, interacaoBanco
 
 def menu_interacao():
-
+    
+    chamafuncao()
+    
     loop = True
 
     while loop != False:
@@ -10,65 +13,26 @@ def menu_interacao():
 
         if continuarSair == 1:
             print("Você Continuou!")
-            print("Verificado se existem tabelas no banco!")
+            print("Verificando se existem tabelas no banco...")
 
             conexao = sqlite3.connect("VamosBricar.sqlite")
             cursor = conexao.cursor()
 
-            comando = "SELECT * FROM sqlite_master where type='table';"
-            cursor.execute(comando)
-
-            tabelas = cursor.fetchall()
-
-            if tabelas:
-                for tabela in tabelas:
-                    print("LISTA DE TABELAS:")
-                    print(f"Tabela:", tabela[1])
-            else:
-                print("Nenhuma Tabela encontrada no banco atual")
+            listarTabela(cursor, conexao)
 
             conexao.commit()
             conexao.close()
          
 
+            interacaoBanco()
 
-            #Criando tabelas
-            criarTabela = int(input(f"Deseja criar uma tabela?\nDigite 1 para Sim e 2 para Não: "))
-            if criarTabela == 1:
-                print("Criando tabelas...")
-            
-            elif criarTabela == 2:
-                print("Ok, não será criado novas tabelas!")
-            
-            else:
-                print("Não foi informado uma opção valida!")
-
-
-            #Deletando tabelas
-            deletarTabela = int(input(f"Deseja deletar uma tabela?\nDigite 1 para Sim e 2 para Não: "))
-
-            if deletarTabela == 1:
-                print("Deletando tabelas...")
-            
-            elif deletarTabela == 2:
-                print("Ok, não será deletado nenhuma tabela!")
-            
-            else:
-                print("Não foi informado uma opção valida!")
-
-
-            loop = True
 
 
 
         elif continuarSair == 2:
 
-            for animacao in range(10):
-                sleep(0.3)
-                print("*")
-                
+            animacao()                
             print("Você Saiu!")
-
             loop = False
 
 
